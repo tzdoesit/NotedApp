b1ab8d255b70575052c815c3dc83787a02e168e7
tzdoesit <tommy.n.zhang@gmail.com>
2026-08-06 22:49:34 -0700

    added SQLite database

--- notes.py
+++ notes.py
@@ -1,20 +1,37 @@
-notes = []
+
+import sqlite3
+
+connection = sqlite3.connect("notes.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY,
+        text TEXT NOT NULL
+    )
+""")
+connection.commit()
+
 
 while True:
     command = input("(a)dd, (l)ist, or (q)uit? ")
 
     if command == "a":
         text = input("Note: ")
-        notes.append(text)
+        cursor.execute("INSERT INTO notes (text) VALUES (?)", (text,))
+        connection.commit()
         print("Saved.")
 
     elif command == "l":
-        for i, note in enumerate(notes):
-            print(f"{i}: {note}")
+        cursor.execute("SELECT id, text FROM notes")
+        for row in cursor.fetchall():
+            print(f"{row[0]}: {row[1]}")
 
     elif command == "q":
         break
 
     else:
         print("Didn't understand that.")
-        
+
+
+connection.close()
